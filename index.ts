b9f4d333d934410115f56d06b46f013f18d5c99e
Jargaltogtokh <Jagaa155@gmail.com>
2025-01-13 18:12:45 +0800

    updated

--- index.ts
+++ index.ts
@@ -1,21 +1,47 @@
 import express, { Request, Response } from "express";
 import { configDotenv } from "dotenv";
 import { MongoClient } from "mongodb";
+import { Schema, model, connect } from "mongoose";
 
 const PORT = 8001;
 const app = express();
 app.use(express.json());
 
 configDotenv();
-const URI = process.env.MONGODB_URI;
 
-console.log(URI);
-//1. Connect to MongoDB
+const connectMongoDB = async () => {
+  const MONGODB_URI = process.env.MONGODB_URI;
+  if (MONGODB_URI) {
+    await connect(MONGODB_URI);
+  }
+};
 
-export const connectToTB= async ()+> {}
+connectMongoDB();
+
+const FOOD_CATEGORY_SCHEMA = new Schema(
+  {
+    categoryName: String,
+  },
+  { timestamps: true }
+);
+
+const FoodCategoryModel = model(
+  "FoodCategory",
+  FOOD_CATEGORY_SCHEMA,
+  "food-category"
+);
 
 app.get("/", async (req: Request, res: Response) => {
-  res.send("Hello from backend");
+  const FoodCategories = await FoodCategoryModel.find();
+  res.json(FoodCategories);
+});
+
+app.get("/create", async (req: Request, res: Response) => {
+  const newItem = await FoodCategoryModel.create({
+    categoryName: "New Food Category created successfully.",
+  });
+
+  res.send({ message: "New Food Category created successfully", newItem });
 });
 
 app.listen(PORT, () => {
