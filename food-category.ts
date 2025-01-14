25b28f2817715fece7626005f8106a0beef3a052
Jargaltogtokh <Jagaa155@gmail.com>
2025-01-14 18:32:32 +0800

    updated

---
+++ food-category.ts
@@ -0,0 +1,54 @@
+import express, { Request, Response } from "express";
+import { Schema, model, connect } from "mongoose";
+
+export const foodCategoryRouter = Router();
+
+foodCategoryRouter.post("/create", async (req: Request, res: Response) => {
+  const { categoryName } = req.body;
+  const newItem = await FoodCategoryModel.create({
+    categoryName,
+  });
+
+  res.send({ message: "New food category created successfully", newItem });
+});
+
+foodCategoryRouter.put("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { categoryName } = req.body;
+
+  const UpdatedCategory = await FoodCategoryModel.findByIdAndUpdate(
+    id,
+    { categoryName },
+    { new: true }
+  );
+  if (UpdatedCategory) {
+    res.json({
+      message: "A food category updated successfully",
+      UpdatedCategory,
+    });
+  }
+});
+
+foodCategoryRouter.delete("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const DeletedCategory = await FoodCategoryModel.findByIdAndDelete(id);
+  if (DeletedCategory) {
+    res.json({
+      message: "A food category deleted successfully",
+      DeletedCategory,
+    });
+  }
+});
+
+foodCategoryRouter.get("/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const SearchedCategory = await FoodCategoryModel.find({ _id: id });
+  if (SearchedCategory) {
+    res.json(SearchedCategory);
+  }
+});
+
+foodCategoryRouter.get("/", async (req: Request, res: Response) => {
+  const data = await FoodCategoryModel.find();
+  res.json(data);
+});
--- index.ts
+++ index.ts
@@ -2,9 +2,15 @@ import express, { Request, Response } from "express";
 import { configDotenv } from "dotenv";
 import { MongoClient } from "mongodb";
 import { Schema, model, connect } from "mongoose";
+import { deepStrictEqual } from "assert";
 
 const PORT = 8001;
 const app = express();
+
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 
 configDotenv();
@@ -17,6 +23,9 @@ const connectMongoDB = async () => {
 };
 
 connectMongoDB();
+
+app.use("/food-category/", foodCategoryRouter);
+app.use("/food/", foodRouter);
 
 const FOOD_CATEGORY_SCHEMA = new Schema(
   {
@@ -31,18 +40,55 @@ const FoodCategoryModel = model(
   "food-category"
 );
 
-app.get("/", async (req: Request, res: Response) => {
-  const FoodCategories = await FoodCategoryModel.find();
-  res.json(FoodCategories);
-});
+// app.post("/food-category/create", async (req: Request, res: Response) => {
+//   const { categoryName } = req.body;
+//   const newItem = await FoodCategoryModel.create({
+//     categoryName,
+//   });
 
-app.get("/create", async (req: Request, res: Response) => {
-  const newItem = await FoodCategoryModel.create({
-    categoryName: "New Food Category created successfully.",
-  });
+//   res.send({ message: "New food category created successfully", newItem });
+// });
 
-  res.send({ message: "New Food Category created successfully", newItem });
-});
+// app.put("/food-category/:id", async (req: Request, res: Response) => {
+//   const { id } = req.params;
+//   const { categoryName } = req.body;
+
+//   const UpdatedCategory = await FoodCategoryModel.findByIdAndUpdate(
+//     id,
+//     { categoryName },
+//     { new: true }
+//   );
+//   if (UpdatedCategory) {
+//     res.json({
+//       message: "A food category updated successfully",
+//       UpdatedCategory,
+//     });
+//   }
+// });
+
+// app.delete("/food-category/:id", async (req: Request, res: Response) => {
+//   const { id } = req.params;
+//   const DeletedCategory = await FoodCategoryModel.findByIdAndDelete(id);
+//   if (DeletedCategory) {
+//     res.json({
+//       message: "A food category deleted successfully",
+//       DeletedCategory,
+//     });
+//   }
+// });
+
+// app.get("/food-category/:id", async (req: Request, res: Response) => {
+//   const { id } = req.params;
+//   const SearchedCategory = await FoodCategoryModel.find({ _id: id });
+//   if (SearchedCategory) {
+//     res.json(SearchedCategory);
+//   }
+// });
+
+// app.get("/food-category/", async (req: Request, res: Response) => {
+//   const data = await FoodCategoryModel.find();
+//   res.json(data);
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
